1edc991aa9147073c61d41b6a4168c6121213ed5
Andrej Benz <hello@benz.dev>
2022-03-26 13:37:39 +0100

    init

---
+++ aspnetcore/Program.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<Db>(options =>
+    options.UseNpgsql(connectionString));
+
+var app = builder.Build();
+
+app.MapGet("/", async (Db db) => await db.Datas.ToListAsync());
+
+app.Run();
+
+class Db: DbContext {
+    public Db(DbContextOptions<Db> options): base(options) {}
+
+    public DbSet<Data> Datas => Set<Data>();
+}
+
+[Table("data")]
+record Data(int id) {
+    public int id {get;set;} = id;
+    public string title {get;set;} = default!;
+    public string content {get;set;} = default!;
+}
---
+++ gin/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+type Post struct {
+	Title   string
+	Content string
+	Id      int
+}
+
+func main() {
+	db, err := pgxpool.Connect(context.Background(), "postgres://postgres@localhost:5432/benchmark")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	gin.SetMode(gin.ReleaseMode)
+
+	r := gin.New()
+
+	r.GET("/", test(db))
+
+	log.Fatal(r.Run())
+}
+
+func test(db *pgxpool.Pool) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		rows, err := db.Query(context.Background(), "SELECT id,title,content FROM data;")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		defer rows.Close()
+
+		posts := []Post{}
+
+		for rows.Next() {
+			post := Post{}
+
+			err := rows.Scan(&post.Id, &post.Title, &post.Content)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
+			}
+
+			posts = append(posts, post)
+		}
+
+		c.JSON(http.StatusOK, posts)
+	}
+}
